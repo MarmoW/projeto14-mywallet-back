2a84f65727efaad8cf101c8f41764805628b7438
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-01-23 12:10:48 -0300

    fixes

--- src/middlewares/ValidateMiddleware.js
+++ src/middlewares/ValidateMiddleware.js
@@ -2,8 +2,6 @@ export default function ValidateSchema(schema) {
     return (req, res, next) => {
       const {error} = schema.validate(req.body, { abortEarly: false })
       
-      console.log(req.body, "opa")
-      console.log(error)
       if (error) {
         const errMessage = error.details.map(err => err.message)
         return res.status(422).send(errMessage)
